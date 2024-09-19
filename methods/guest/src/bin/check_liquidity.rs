b931d973fbb16fe99cf9fe7a9266ca8287e0e3bd
Timo Neumann <timoneumann@192.168.1.242>
2024-09-19 17:32:46 +0100

    fmt

--- methods/guest/src/bin/check_liquidity.rs
+++ methods/guest/src/bin/check_liquidity.rs
@@ -58,9 +58,7 @@ fn main() {
     // Journal is encoded using Solidity ABI for easy decoding in the app contract.
     let journal = Journal {
         commitment: env.block_commitment(),
-        commitment: env.block_commitment(),
         liquidity: returns._1,
-        user: account
         user: account
     };
     env::commit_slice(&journal.abi_encode());
