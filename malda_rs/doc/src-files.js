b3de7f4e184ec0c10fc49ee784f381532873e4f2
Timo Neumann <timoneumann@Timos-MacBook-Pro.local>
2024-11-15 14:08:26 +0000

    more tests

--- malda_rs/doc/src-files.js
+++ malda_rs/doc/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["malda_rs",["",[["methods",[["guest",[["guest_utils",[["src",[],["cryptography.rs","types.rs","validators.rs"]]]]]]]]],["constants.rs","lib.rs","viewcalls.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["malda_rs",["",[["methods",[["guest",[["guest_utils",[["src",[],["constants.rs","cryptography.rs","types.rs","validators.rs"]]]]]]]]],["constants.rs","lib.rs","viewcalls.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[162]}
+//{"start":36,"fragment_lengths":[177]}
