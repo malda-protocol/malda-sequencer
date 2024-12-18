c73aed2c6e98358fbbb40ea6c5d6c5aab68e4399
team0 <team0.bctech@gmail.com>
2024-12-18 17:39:45 +0100

    bugfix

--- methods/guest/guest_utils/src/validators_ethereum_light_client.rs
+++ methods/guest/guest_utils/src/validators_ethereum_light_client.rs
@@ -372,7 +372,7 @@ pub fn validate_ethereum_env_via_sync_committee(
 
     let verified_root = B256::new(verified_root.0);
 
-    let new_checkpoint = B256::new(l1_chain_builder.last_checkpoint.unwrap().0);
+    let new_checkpoint = l1_chain_builder.last_checkpoint.map_or_else(|| B256::from(checkpoint.0), |last| B256::new(last.0));
 
     (verified_root, new_checkpoint)
 }
